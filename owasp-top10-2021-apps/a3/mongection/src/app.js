2c6b483c58b7f45c1ce175480de59c6e3cca1e46
ZeroPath <noreply@zeropath.com>
2025-10-20 06:48:50 +0000

    fix: prevent user data leak in successful login response

--- owasp-top10-2021-apps/a3/mongection/src/app.js
+++ owasp-top10-2021-apps/a3/mongection/src/app.js
@@ -43,9 +43,9 @@ server.post("/login", async (request, response) => {
 
         console.log(user.length)
 
-        if(user.length == 0) { response.send('Bad Credentials'); }
+        if(user.length == 0) { return response.send('Bad Credentials'); }
 
-        response.send("<h1>Hello, Welcome Again!</h1><h3>" + user + "</h3>");
+        return response.send("<h1>Hello, Welcome Again!</h1>");
     }
    
     catch(error) { throw error; }
