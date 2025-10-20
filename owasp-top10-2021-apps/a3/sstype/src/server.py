4fd15227b6f98f96830c792ffddbb0c97b27d98d
ZeroPath <noreply@zeropath.com>
2025-10-20 18:36:07 +0000

    fix: sanitize user input to prevent XSS vulnerability

--- owasp-top10-2021-apps/a3/sstype/src/server.py
+++ owasp-top10-2021-apps/a3/sstype/src/server.py
@@ -2,6 +2,7 @@ import tornado.template
 import tornado.ioloop
 import tornado.web
 import os
+import tornado.escape
 
 TEMPLATE = open(os.path.join(os.path.dirname(__file__)) + "/public/index.html", 'r').readlines()
 
@@ -13,9 +14,10 @@ class MainHandler(tornado.web.RequestHandler):
 
     def get(self):
         name = self.get_argument('name', '')
-        template_data = tmpl.replace("NAMEHERE",name)
+        esc_name = tornado.escape.xhtml_escape(name)
+        template_data = tmpl.replace("NAMEHERE", esc_name)
         t = tornado.template.Template(template_data)
-        self.write(t.generate(name=name))
+        self.write(t.generate(name=esc_name))
 
 application = tornado.web.Application([
     (r"/", MainHandler),
