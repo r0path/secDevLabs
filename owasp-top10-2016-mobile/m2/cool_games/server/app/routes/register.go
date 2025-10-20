e2fde487d62fe767e2aaaed4434e21191d783e88
ZeroPath <noreply@zeropath.com>
2025-10-20 06:49:40 +0000

    fix: prevent duplicate account creation on database errors

--- owasp-top10-2016-mobile/m2/cool_games/server/app/routes/register.go
+++ owasp-top10-2016-mobile/m2/cool_games/server/app/routes/register.go
@@ -2,11 +2,13 @@ package routes
 
 import (
 	"net/http"
+	"errors"
 
 	"github.com/globocom/secDevLabs/owasp-top10-2016-mobile/m2/cool_games/server/app/db"
 	"github.com/globocom/secDevLabs/owasp-top10-2016-mobile/m2/cool_games/server/app/types"
 	"github.com/globocom/secDevLabs/owasp-top10-2016-mobile/m2/cool_games/server/app/util"
 	"github.com/labstack/echo"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Register tries to register a new user into the database
@@ -24,6 +26,13 @@ func Register(c echo.Context) error {
 		return c.JSON(http.StatusConflict, "User already exists!")
 	}
 
+	// If the error is something other than "no documents found", it's a real DB error
+	if err != nil {
+		if !errors.Is(err, mongo.ErrNoDocuments) {
+			return c.JSON(http.StatusInternalServerError, "Database error while checking username, please try again later")
+		}
+	}
+
 	hashedPassword, salt, err := util.Hash(attemptPassword)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, "Error hashing user password, please try again later")
@@ -33,6 +42,15 @@ func Register(c echo.Context) error {
 
 	err = db.InsertOneUser(newUser)
 	if err != nil {
+		// handle duplicate key just in case a race created the user after the previous check
+		var writeErr mongo.WriteException
+		if errors.As(err, &writeErr) {
+			for _, we := range writeErr.WriteErrors {
+				if we.Code == 11000 {
+					return c.JSON(http.StatusConflict, "User already exists!")
+				}
+			}
+		}
 		return c.JSON(http.StatusInternalServerError, "Error registering new user, please try again later")
 	}
 
