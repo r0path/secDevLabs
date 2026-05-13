86b1efb92b90080acf2255c018771872a3f3e7c2
ZeroPath <security@zeropath.com>
2026-05-13 01:46:40 +0000

    Fix PHP RCE: replace preg_replace /e modifier with preg_replace_callback

--- owasp-top10-2021-apps/a6/cimentech/app/html/modules/filter/filter.api.php
+++ owasp-top10-2021-apps/a6/cimentech/app/html/modules/filter/filter.api.php
@@ -202,7 +202,9 @@ function callback_filter_settings($form, &$form_state, $filter, $format, $defaul
  */
 function callback_filter_prepare($text, $filter, $format, $langcode, $cache, $cache_id) {
   // Escape <code> and </code> tags.
-  $text = preg_replace('|<code>(.+?)</code>|se', "[codefilter_code]$1[/codefilter_code]", $text);
+  $text = preg_replace_callback('|<code>(.+?)</code>|s', function($matches) {
+    return '[codefilter_code]' . $matches[1] . '[/codefilter_code]';
+  }, $text);
   return $text;
 }
 
@@ -234,7 +236,9 @@ function callback_filter_prepare($text, $filter, $format, $langcode, $cache, $ca
  * @ingroup callbacks
  */
 function callback_filter_process($text, $filter, $format, $langcode, $cache, $cache_id) {
-  $text = preg_replace('|\[codefilter_code\](.+?)\[/codefilter_code\]|se', "<pre>$1</pre>", $text);
+  $text = preg_replace_callback('|\[codefilter_code\](.+?)\[/codefilter_code\]|s', function($matches) {
+    return '<pre>' . $matches[1] . '</pre>';
+  }, $text);
 
   return $text;
 }
