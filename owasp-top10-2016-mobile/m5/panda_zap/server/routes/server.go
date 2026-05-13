7fb9c5e9dfe62249beb3bb966559812d7397f4a1
ZeroPath <security@zeropath.com>
2026-05-13 01:47:06 +0000

    Fix unauthenticated user data exposure: require JWT auth on GET /user/:name

--- owasp-top10-2016-mobile/m5/panda_zap/server/routes/server.go
+++ owasp-top10-2016-mobile/m5/panda_zap/server/routes/server.go
@@ -27,7 +27,11 @@ func (es *EchoServer) SetGenericRoutes() {
 
 	// user routes
 	es.Engine.POST("/user", es.RegisterUser)
-	es.Engine.GET("/user/:name", es.GetUser)
+
+	// restricted user routes
+	u := es.Engine.Group("/user")
+	u.Use(middleware.JWT([]byte(es.Settings.GetString("jwt_secret"))))
+	u.GET("/:name", es.GetUser)
 
 	// restricted messages routes
 	r := es.Engine.Group("/messages")
